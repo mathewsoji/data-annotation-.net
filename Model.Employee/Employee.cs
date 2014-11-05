50dd57cd0ee11d3033abde90f9b16f173fefa95d
mathewsoji <soji.mathew@outlook.com>
2014-11-05 20:43:35 +0530

    Data Annotation in .NET
    Basic introduction to Data Annotation in .NET Framework

---
+++ Model.Employee/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace Model.Employee
+{
+     
+      public class Employee
+    {
+        [Required (ErrorMessage="Employee {0} is required")]
+        [StringLength (100,MinimumLength=3,
+        ErrorMessage="Name Should be minimum 3 characters and a maximum of 100 characters")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Range(18,99, ErrorMessage="Age should be between 18 and 99")]
+        public int Age { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+    
+}
---
+++ TestEmployeeValidation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Employee;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace TestEmployeeValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Employee class Validation");
+            Console.WriteLine("---------------------------\n");
+
+            Employee objEmployee = new Employee();
+            objEmployee.Name = "sa";
+            objEmployee.Age = 12;
+            objEmployee.PhoneNumber = "1234as";
+            objEmployee.Email = "test@re";
+
+
+            ValidationContext context = new ValidationContext(objEmployee, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+            bool valid = Validator.TryValidateObject(objEmployee, context, results, true);
+
+            if (!valid)
+            {
+                foreach (ValidationResult vr in results)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("Member Name :{0}", vr.MemberNames.First());
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("   ::  {0}{1}", vr.ErrorMessage, Environment.NewLine);
+
+                }
+
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
